6caae8a94dfad49b1ffb22fa99ee544e7cc8380d
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2021-10-03 19:57:29 -0400

    retweet

--- src/Components/Tweets.js
+++ src/Components/Tweets.js
@@ -2,24 +2,53 @@ import React, {useState} from 'react'
 import "../css/Tweets.css"
 
 const Tweets = props => {
-    const [likes,setLikes] = useState(0)
-    return ( 
-        <div className="tweets">
-            <p>{props.title}</p>
-            <p>{props.content}</p>
-            <p>{props.author}</p>
-            <p>{props.date}</p>
-            <p>{props.likes}</p>
-            <p>{props.retweets}</p>
-            <h1>Likes: {likes}</h1>
-            <button onClick={()=>{
-                setLikes(likes+1)
-            }}>Add LIKe</button>
-            <button onClick={()=>{
-                setLikes(likes-1)
-            }}>Delete Like</button>
-        </div>
-    );
+    const [likes,setLikes] = useState(0);
+    const [retweet,setRetweet] = useState('False');
+    if (retweet === 'False') {
+        return ( 
+            <div className="tweets">
+                <p>{props.title}</p>
+                <p>{props.content}</p>
+                <p>{props.author}</p>
+                <p>{props.date}</p>
+                <p>{props.likes}</p>
+                <p>{props.retweets}</p>
+                <h1>Likes: {likes}</h1>
+                <button onClick={()=>{
+                    setLikes(likes+1)
+                }}>Add LIKe</button>
+                <button onClick={()=>{
+                    setLikes(likes-1)
+                }}>Delete Like</button>
+                <h1>Retweeted?: {retweet}</h1>
+                <button onClick={()=>{
+                    setRetweet('True')
+                }}>Retweet</button>
+            </div>
+        ); }
+    else if (retweet === 'True'){
+        return ( 
+            <div className="tweets">
+                <p>{props.title}</p>
+                <p>{props.content}</p>
+                <p>{props.author}</p>
+                <p>{props.date}</p>
+                <p>{props.likes}</p>
+                <p>{props.retweets}</p>
+                <h1>Likes: {likes}</h1>
+                <button onClick={()=>{
+                    setLikes(likes+1)
+                }}>Add LIKe</button>
+                <button onClick={()=>{
+                    setLikes(likes-1)
+                }}>Delete Like</button>
+                <h1>Retweeted?: {retweet}</h1>
+                <button onClick={()=>{
+                    setRetweet('False')
+                }}>Undo Retweet</button>
+            </div>
+        ); 
+    }
 };
 
 export default Tweets;
