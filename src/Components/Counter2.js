20669bfaf583e88989897427017969e6fcf80872
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2021-10-03 19:50:15 -0400

    tweetlikes

--- src/Components/Counter2.js
+++ src/Components/Counter2.js
@@ -2,8 +2,17 @@ import React, {useState} from 'react';
 
 const Counter2 = () => {
     const [count,setCount]=useState(0);
+    const [str,setstr]=useState('false')
+    const changestring = () => {
+        if(str==='false'){
+            setstr('true')
+        }
+        else if (str ==='true')
+         setstr('false')
+    }
     return (
         <div>
+            <h1>String: {str}</h1>
             <h1>Counter: {count} </h1>
             <button onClick={()=>{
                 setCount(count+1);
@@ -14,6 +23,10 @@ const Counter2 = () => {
             <button onClick={()=>{
                 console.log('hello')
             }}>Hello</button>
+            <button onClick={()=>{
+                setCount(0)
+            }}>Set to zero</button>
+            <button onClick={changestring}>change string</button>
         </div>
     )
 }
--- src/Components/Tweets.js
+++ src/Components/Tweets.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../css/Tweets.css"
 
 const Tweets = props => {
+    const [likes,setLikes] = useState(0)
     return ( 
         <div className="tweets">
             <p>{props.title}</p>
@@ -10,6 +11,13 @@ const Tweets = props => {
             <p>{props.date}</p>
             <p>{props.likes}</p>
             <p>{props.retweets}</p>
+            <h1>Likes: {likes}</h1>
+            <button onClick={()=>{
+                setLikes(likes+1)
+            }}>Add LIKe</button>
+            <button onClick={()=>{
+                setLikes(likes-1)
+            }}>Delete Like</button>
         </div>
     );
 };
