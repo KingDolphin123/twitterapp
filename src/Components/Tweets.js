0fdb9415cd2c9dba10aa9d583d93a52611e1e6d2
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2021-10-04 17:24:05 -0400

    ternary

--- src/Components/Tweets.js
+++ src/Components/Tweets.js
@@ -3,93 +3,22 @@ import "../css/Tweets.css"
 import whiteheart from "../css/1whiteheart.png"
 import redheart from "../css/1redheart.png"
 
-
 const Tweets = props => {
-    const [likes,setLikes] = useState('False');
-    const [retweet,setRetweet] = useState('False');
-    if (retweet === 'False') {
-        if(likes === 'False') {
-            return ( 
-                <div className ="tweets">
-                    <p>{props.title}</p>
-                    <p>{props.content}</p>
-                    <p>{props.author}</p>
-                    <p>{props.date}</p>
-                    <p>{props.likes}</p>
-                    <p>{props.retweets}</p>
-                    <h1>Liked?: {likes}</h1>
-                    <input className = 'image' type='image' name = 'Add Like' src={whiteheart} alt = 'whiteheart' onClick={()=>{
-                        setLikes('True')
-                    }}/>
-                    <h1>Retweeted?: {retweet}</h1>
-                    <button onClick={()=>{
-                        setRetweet('True')
-                    }}>Retweet</button>
-                </div>
-            ); 
-        }
-        if(likes==='True'){
-            return ( 
-                <div className ="tweets">
-                    <p>{props.title}</p>
-                    <p>{props.content}</p>
-                    <p>{props.author}</p>
-                    <p>{props.date}</p>
-                    <p>{props.likes}</p>
-                    <p>{props.retweets}</p>
-                    <h1>Liked?: {likes}</h1>
-                    <input className = 'image' type='image' name = 'Add Like' src={redheart} alt = 'redheart' onClick={()=>{
-                        setLikes('False')
-                    }}/>
-                    <h1>Retweeted?: {retweet}</h1>
-                    <button onClick={()=>{
-                        setRetweet('True')
-                    }}>Retweet</button>
-                </div>
-            ); 
-        }
-    }
-    else if (retweet === 'True') {
-        if(likes === 'False') {
-            return ( 
-                <div className ="tweets">
-                    <p>{props.title}</p>
-                    <p>{props.content}</p>
-                    <p>{props.author}</p>
-                    <p>{props.date}</p>
-                    <p>{props.likes}</p>
-                    <p>{props.retweets}</p>
-                    <h1>Liked?: {likes}</h1>
-                    <input className = 'image' type='image' name = 'Add Like' src={whiteheart} alt = 'whiteheart' onClick={()=>{
-                        setLikes('True')
-                    }}/>
-                    <h1>Retweeted?: {retweet}</h1>
-                    <button onClick={()=>{
-                        setRetweet('False')
-                    }}>Undo Retweet</button>
-                </div>
-            ); 
-        }
-        if(likes==='True'){
-            return ( 
-                <div className ="tweets">
-                    <p>{props.title}</p>
-                    <p>{props.content}</p>
-                    <p>{props.author}</p>
-                    <p>{props.date}</p>
-                    <p>{props.likes}</p>
-                    <p>{props.retweets}</p>
-                    <h1>Liked?: {likes}</h1>
-                    <input className = 'image' type='image' name = 'Add Like' src={redheart} alt = 'redheart' onClick={()=>{
-                        setLikes('False')
-                    }}/>
-                    <h1>Retweeted?: {retweet}</h1>
-                    <button onClick={()=>{
-                        setRetweet('False')
-                    }}>Undo Retweet</button>
-                </div>
-            ); 
-        }
-    }
+    const [likes,setLikes] = useState(false);
+    const [retweet,setRetweet] = useState(false);
+    return (
+        <div className ="tweets">
+            <p>{props.title}</p>
+            <p>{props.content}</p>
+            <p>{props.author}</p>
+            <p>{props.date}</p>
+            <p>{props.likes}</p>
+            <p>{props.retweets}</p>
+            <h1>Liked: {likes? "True":"False"}</h1>
+            {likes ? <input className = 'image' type='image' name = 'Add Like' src={redheart} alt = 'redheart' onClick={()=>{setLikes(false)}}/> : <input className = 'image' type='image' name = 'Add Like' src={whiteheart} alt = 'whiteheart' onClick={()=>{setLikes(true)}}/>}
+            <h1>Retweeted: {retweet? "True":"False"}</h1>
+            {retweet ? <button onClick={()=>{setRetweet(false)}}>Undo Retweet</button> : <button onClick={()=>{setRetweet(true)}}>Retweet</button>}
+        </div>
+    )   
 }
 export default Tweets;
