4e5e8f6f27e3ac58e890135a64bf00b0803f2193
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2021-10-19 10:39:58 -0400

    d

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Tweets from './Components/Tweets';
 import "./css/App.css";
 
 function App() {
-  let sampletweet1 = {content:'hello',author:'ko',date:'918ce',likes:3,retweets:1}
-  let sampletweet2 = {content:'hi',author:'salami',date:'2020',likes:1,retweets:5}
-  let tweetArr = [sampletweet1,sampletweet2]
+  let sampletweet1 = {content:'hello',author:'ko',date:'918ce',likes:3,retweets:1};
+  let sampletweet2 = {content:'hi',author:'salami',date:'2020',likes:1,retweets:5};
+  let tweetArr = [sampletweet1,sampletweet2];
+  let filterArray1 = [];
   const [inputStateContent, setInputStateContent] = useState('');
   const [inputStateAuthor, setInputStateAuthor] = useState('');
   const [inputStateDate, setInputStateDate] = useState('');
   const [Array, setArray] = useState(tweetArr);
   const [filter, setFilter] = useState("");
+  const [filterArray, setFilterArray] = useState(filterArray1);
+  const [filterBool, setFilterBool] = useState(false)
   const submit = () => {
     if (inputStateContent.length > 0 && inputStateAuthor.length > 0 && inputStateDate.length > 0) {
       setArray(tweetArr => [...tweetArr,{content: inputStateContent, author: inputStateAuthor, date: inputStateDate, likes:0, retweets:0}])
@@ -21,16 +24,23 @@ function App() {
       setInputStateDate("");
     };
   };
-  const forEachFilter = (e) => {
-    let eArray = [e]
-    if (!eArray.includes(filter)){
-      Array.splice(Array[e],1)
-    }
+  // const forEachFilter = (e) => {
+  //   let eArray = [e]
+  //   if (eArray.includes(filter)){
+  //     setFilterArray(filterArray1 => [...filterArray1, eArray])
+  //   }
+  // }
+  // const filtertweets = () => {
+  //   Array.forEach(function(element){
+  //     forEachFilter(element)
+  //   })
+  //   setFilterBool(!filterBool)
+  // }
+  const filterfunc = (e) => {
+    
   }
   const filtertweets = () => {
-    Array.forEach(function(element){
-      forEachFilter(element)
-    })
+    let filteredtweets = Array.filter(filterfunc)
   }
   return (
     <div className = "bg">
@@ -44,9 +54,14 @@ function App() {
       <button type = 'submit' onClick={filtertweets}>FILTER</button>
       <p>{inputStateContent}{inputStateAuthor}{inputStateDate}</p>
       <div className="app">
-          {Array.map((tweet,i) => (
+        {filterBool?
+          (filterArray.map((tweet,i) => (
             <Tweets key={i+1} content={tweet.content} author={tweet.author} date={tweet.date} likes={tweet.likes} retweets={tweet.retweets}/>
-          ))}
+          ))):
+          (Array.map((tweet,i) => (
+            <Tweets key={i+1} content={tweet.content} author={tweet.author} date={tweet.date} likes={tweet.likes} retweets={tweet.retweets}/>
+          )))
+        }
       </div>
     </div>
   );
